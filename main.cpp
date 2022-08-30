6399f4a4e39b5ea5780ebb5dc4cf9df5479e24c9
khannaradhika08 <78994856+khannaradhika08@users.noreply.github.com>
2022-08-30 19:02:19 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-class Student{
-private:
-    int classofstudent;
-    int salaryofparents;
+class numbers
+{
+    int a;
 public:
-    int rollnumber;
-    string name;
-    void set_data(int classofstudent1, int salaryofparents1);
-    void get_data();
+    void read_num();
+    int cube_num();
+    int sq_num();
+    void display_num();
 };
-void Student::set_data(int classofstudent1, int salaryofparents1){
-classofstudent=classofstudent1;
-salaryofparents=salaryofparents1;
-}
 
-void Student :: get_data(){
-cout<<"the class of the student is "<<classofstudent<<endl;
-cout<<"the salary of the parents of the student is "<<salaryofparents<<endl;
-cout<<"the name of the student is "<<name<<endl;
-cout<<"the roll number of the student is "<<rollnumber<<endl;
+void numbers :: read_num()
+{
+    cout<<"Enter a number: "<<endl;
+    cin>>a;
+}
+int numbers :: cube_num()
+{
+    return a*a*a;
+}
+int numbers :: sq_num()
+{
+    return a*a;
+}
+void numbers :: display_num()
+{
+    int sq;
+    int cube;
+    sq=sq_num();
+    cube=cube_num();
+    cout<<"the square of the number is: "<<sq<<endl;
+    cout<<"the cube of the number is: "<<cube<<endl;
 }
 int main()
 {
-    Student rads;
-    rads.rollnumber=23;
-    rads.name="Radhika";
-    rads.set_data(9,5000);
-    rads.get_data();
-
+    numbers n;
+    n.read_num();
+    n.display_num();
     return 0;
 }
